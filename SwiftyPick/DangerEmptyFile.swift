3eaf10a79d223d501f23720fac097e5444249b53
Manu Herrera <manuherrera8@gmail.com>
2021-08-25 12:08:47 -0300

    🎨 Add Danger to the project Also add github actions to run Danger, Swiftlint, build, and test

---
+++ SwiftyPick/DangerEmptyFile.swift
@@ -0,0 +1,11 @@
+//
+//  DangerEmptyFile.swift
+//  SwiftyPick
+//
+//  Created by Manu Herrera on 25/08/2021.
+//
+
+import Foundation
+
+// [DO NOT REMOVE THIS FILE]
+// It's only used to make sure the `swift build` command builds correctly using Danger.
