4727f6bfea3c1b5f73b129da38d3bbb8be663dbe
Manu Herrera <manuherrera8@gmail.com>
2021-08-24 22:51:23 -0300

    Create new empty Xcode project

---
+++ SwiftyPick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyPick
+//
+//  Created by Manu Herrera on 24/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
