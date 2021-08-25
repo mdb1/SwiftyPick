aa84ad9c47d96ba3d675ab4740b943e124f1d2b9
Manu Herrera <manuherrera8@gmail.com>
2021-08-24 22:58:30 -0300

    Delete all references to the Main Storyboard

--- SwiftyPick/ViewController.swift
+++ SwiftyPick/ViewController.swift
@@ -11,9 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
     }
-
 
 }
 
