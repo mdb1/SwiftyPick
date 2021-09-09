485733a69bab5f919c390071ef6db2d3ee685aef
Manu Herrera <manuherrera8@gmail.com>
2021-09-08 23:55:37 -0300

    🧪 Add unit tests for BaseView and ColorPaletteScreen ✂️ Remove the post-build script due to problems with Xcode generating test reports 🐛 Minor change in hexString() method for UIColor

---
+++ SwiftyPickTests/Presentation/Screen/ColorPalette/ColorPaletteScreenTests.swift
@@ -0,0 +1,44 @@
+//
+//  ColorPaletteScreenTests.swift
+//  SwiftyPickTests
+//
+//  Created by Manu Herrera on 08/09/2021.
+//
+
+@testable import SwiftyPick
+import XCTest
+
+class ColorPaletteScreenTests: XCTestCase {
+    private var presenter: ColorPalettePresenting!
+    private var vc: ColorPaletteViewController!
+    private var view: ColorPaletteView!
+
+    override func setUp() {
+        super.setUp()
+        presenter = ColorPalettePresenter()
+        vc = ColorPaletteViewController(presenter: presenter)
+        view = ColorPaletteView(palette: .default)
+        vc.loadView()
+    }
+
+    override func tearDown() {
+        defer { super.tearDown() }
+        presenter = nil
+        vc = nil
+        view = nil
+    }
+
+    func testVcAndPresenter() {
+        presenter.delegate = vc
+        XCTAssert(presenter.delegate === vc)
+    }
+
+    func testVcAndView() {
+        view.delegate = vc
+        XCTAssert(view.delegate === vc)
+    }
+
+    func testGetPalette() {
+        XCTAssertEqual(presenter.getPaletteView().allColors, UIColor.Palette.flashy.allColors)
+    }
+}
